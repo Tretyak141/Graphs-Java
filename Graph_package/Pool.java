61dd10581dde681e2501cadbc0cbde3c16366402
Tretyak141 <andreytretyakov0488@yandex.ru>
2024-03-12 12:27:53 +0300

    Delete extra-sorting in adding edge

--- Graph_package/Pool.java
+++ Graph_package/Pool.java
@@ -9,26 +9,15 @@ public class Pool {
     protected ArrayList<Integer> connected;
 
 
-    public double show_volume(){
-        return this.volume;
-    }
-
     Pool(){
         volume = 0;
         passed = false;
         distributed = false;
         connected = new ArrayList<>();
     }
-    private int cmp(int num1,int num2)
-    {
-        if (num1 > num2) return 1;
-        if (num2 > num1) return -1;
-        return 0;
-    }
     public void add_edge(int num)
     {
         this.connected.add(num);
-        this.connected.sort(this::cmp);
     }
     public void delete_edge(Integer num) {
         this.connected.remove(num);
